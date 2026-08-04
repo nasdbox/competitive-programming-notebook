5633a98e8ea9745853936ee33acf3706967c3423
liveuser <154728897+nasdbox@users.noreply.github.com>
2026-08-05 04:39:38 +0530

    Add functions to calculate GCD and LCM

---
+++ algo.cpp
@@ -0,0 +1,13 @@
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    if (a == 0 or b == 0) return 0;
+    else return (a/gcd(a, b)) * b;
+}
